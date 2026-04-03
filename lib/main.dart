dcc0eacbdaea9aca4a35f8663d83a8aba976d0ed
abdomostafa <https://github.com/Abdo-Mostafa2458/flutter_quran_app.git>
2026-04-03 13:57:42 +0200

    ++finised install env firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:provider/provider.dart';
@@ -7,7 +8,14 @@ import 'package:todo_app/Provider/provider.dart';
 import 'package:todo_app/home_screen.dart';
 import 'package:todo_app/splash_screen.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MultiProvider(providers: [
     ChangeNotifierProvider(
       create: (context) => AppSettings(),
